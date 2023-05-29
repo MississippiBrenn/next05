13c8e0f16e2632a716872ea4de5f00f5c0cfbcbd
Mississippi Brennan <mbrennan@blueorigin.com>
2023-05-29 14:18:51 -0700

    wiki search

---
+++ app/types.d.ts
@@ -0,0 +1,18 @@
+//with this ts does not require imports because in tsconfig file it loos for .ts files to pull in
+
+type Result = {
+  pageid: string;
+  title: string;
+  extract: string;
+  thumbnail?: {
+    source: string;
+    width: number;
+    height: number;
+  };
+};
+
+type SearchResult = {
+  query?: {
+    pages?: Result[];
+  };
+};
